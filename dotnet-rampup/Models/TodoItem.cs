ce34aa57b5b173775744d6a3b8fbc1db9d3c256f
jackedgar <JEdgar@bofifederalbank.com>
2023-10-30 13:53:50 -0700

    initial progress for todo

---
+++ dotnet-rampup/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_rampup.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
